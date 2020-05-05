8abcf5023dba5d5d112513e8e41cb728db1ab2ef
julienudev <julien.udev@gmail.com>
2020-05-05 10:40:29 +0200

    ajout age dans export client csv

--- src/main/java/com/example/demo/service/export/ExportClientCSVService.java
+++ src/main/java/com/example/demo/service/export/ExportClientCSVService.java
@@ -2,10 +2,14 @@ package com.example.demo.service.export;
 
 import com.example.demo.entity.Client;
 import com.example.demo.repository.ClientRepository;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Service
@@ -17,11 +21,16 @@ public class ExportClientCSVService {
 
     public void exportAll(PrintWriter writer) {
 
-        //génération d'un fichier CSV exemples avec 2 colonnes et 4 lignes
-
+        //génération d'un fichier CSV exemples avec 3 colonnes et xClients lignes
+        LocalDate now = LocalDate.now();
         List<Client> allClients = clientRepository.findAll();
+
+        //Creation headers liste tous les clients
+        writer.println("Nom"+";"+"Prenom"+";"+"Age");
+
         for (Client client : allClients) {
-            writer.println(client.getNom() + ";" + client.getPrenom());
+            writer.println(client.getNom() + ";" + client.getPrenom()+
+                    ";" + (now.getYear() - client.getDateNaissance().getYear())+" ans");;
 
         }
 
